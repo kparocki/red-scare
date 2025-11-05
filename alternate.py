ed3b543208bb6f613d8a70974fc12f310d7e348c
Gustav Kristensen <gustavkrist@gmail.com>
2025-11-05 14:01:54 +0100

    alternate.py: run on all instances

--- alternate.py
+++ alternate.py
@@ -1,9 +1,28 @@
 from temp import bfs
 
 
-def alternate(s: str, t: str, G: dict[str, set[str]], red: dict[str, bool]) -> bool:
+def alternate(s: str, t: str, G: dict[str, set[str]], red: set[str]) -> bool:
     for node, neighbors in G.items():
         for neighbor in neighbors.copy():
             if (node in red) == (neighbor in red):
                 G[node].remove(neighbor)
     return bool(bfs(G, s, t))
+
+
+def main():
+    from temp import grapher
+    import os
+
+    files = os.listdir("data")
+    files.remove("README.md")
+
+    def run_alternate(filename):
+        G, _, (s, t), red = grapher(filename)
+        return alternate(s, t, G, red)
+
+    for file in files:
+        print(f"{file}: alternate = {run_alternate(file)}")
+
+
+if __name__ == "__main__":
+    main()
